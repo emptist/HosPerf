e2e77a2168f4995135a81f11fa1eeeb33e106869
emptist <jigme1968@gmail.com>
2020-12-26 00:30:21 +0800

    Initial Commit

---
+++ Shared/HosPerfApp.swift
@@ -0,0 +1,17 @@
+//
+//  HosPerfApp.swift
+//  Shared
+//
+//  Created by jk on 2020/12/26.
+//
+
+import SwiftUI
+
+@main
+struct HosPerfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
