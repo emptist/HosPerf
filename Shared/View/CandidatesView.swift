13fbe0de4bd19280fbccd7409d94e8951d4b976a
emptist <jigme1968@gmail.com>
2020-12-27 10:40:29 +0800

    candidate view

---
+++ Shared/View/CandidatesView.swift
@@ -0,0 +1,74 @@
+//
+//  CandidatesView.swift
+//  HosPerf
+//
+//  Created by jk on 2020/12/27.
+//
+
+import SwiftUI
+
+struct CandidatesView: View {
+    @ObservedObject var archive: Archive
+    @State var inputMode = true
+    
+    var body: some View {
+        NavigationView {
+            // A list of Students
+            CandidateList(archive: archive, inputMode: $inputMode)
+            
+            // this is for iPad and MacOS
+            Text("Please Add Some Candidates")
+                .font(.title)
+        }
+    }
+}
+
+
+
+struct CandidateList: View {
+    @ObservedObject var archive: Archive
+    @Binding var inputMode: Bool //= true
+    
+    var body: some View {
+        VStack {
+            withAnimation() {
+                Toggle(isOn: $inputMode) {
+                    Text("editMode")
+                }
+            }
+            
+            List {
+                ForEach(0..<archive.candidates.count, id:\.self) { idx in
+                    ItemCell(archive: archive, idx: idx, imputMode: $inputMode)
+                }
+            }
+            .navigationTitle("考核对象")
+        }
+    }
+}
+
+
+
+
+struct ItemCell: View {
+    @ObservedObject var archive: Archive
+    var idx: Int
+    @Binding var imputMode: Bool //= true
+    var body: some View {
+        NavigationLink(
+            destination: DetailView(candidate: $archive.candidates[idx], inputMode: $imputMode),
+            label: {
+                Text("\(archive.candidates[idx].单位名称)")
+            })
+    }
+}
+
+
+
+
+struct CandidatesView_Previews: PreviewProvider {
+    static var previews: some View {
+        CandidatesView(archive: Archive())
+    
+    }
+}
--- Shared/View/ContentView.swift
+++ Shared/View/ContentView.swift
@@ -7,56 +7,20 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
-    @ObservedObject var archive: Archive
-
     var body: some View {
+        Text("hello world")
+    }
+}
+
         
-        NavigationView {
-            // A list of Students
-            CandidateList(archive: archive)
-            
-            // this is for iPad and MacOS
-            Text("Please Add Some Students")
-                .font(.largeTitle)
-        }
 
-//        VStack {
-//            Text("\(archive.candidates[0].单位名称)")
-//                .font(.largeTitle)
-//                .fontWeight(.bold)
-//                .foregroundColor(Color.orange)
-//                .multilineTextAlignment(.center)
-//                .shadow(radius: 10)
-//            ResultView(archive: archive)
-//        }
-    }
-}
 
-struct CandidateList: View {
-    @ObservedObject var archive: Archive
-    
-    var body: some View {
-        List {
-            ForEach(0..<archive.candidates.count, id:\.self) { idx in
-                NavigationLink(
-                    destination: DetailView(candidate: $archive.candidates[idx]),
-                    label: {
-                        Text("\(archive.candidates[idx].单位名称)")
-                    })
-            
-            }
-        }
-        .navigationTitle("考核对象")
-
-    }
-    
-    
-    
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(archive: Archive())
+        ContentView()
     }
 }
